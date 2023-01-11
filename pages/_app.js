de2d110d0926dda8cdab6c36931af603355a2d0b
RogelioLB <rogelio20052011@gmail.com>
2023-01-11 14:04:53 -0700

    Google-Site

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,22 @@
-import '../styles/globals.css'
-import {Provider} from "react-redux"
-import store from '../store'
+import "../styles/globals.css";
+import { Provider } from "react-redux";
+import store from "../store";
+import { DefaultSeo } from "next-seo";
 
 function MyApp({ Component, pageProps }) {
-  return <Provider store={store}>
-    <Component {...pageProps} />
+  return (
+    <Provider store={store}>
+      <DefaultSeo
+        additionalMetaTags={[
+          {
+            name: "google-site-verification",
+            content: "k8T0bep__3ADBGgZIZB6NJOQu3m-NwmLgdHsASvK10U",
+          },
+        ]}
+      />
+      <Component {...pageProps} />
     </Provider>
+  );
 }
 
-export default MyApp
+export default MyApp;
