84c6e598aeaa827e42cf84a011290196b43baa0d
RogelioLB <rogelio20052011@gmail.com>
2023-02-02 19:04:36 -0700

    URL issues

--- pages/server-sitemap.xml/index.tsx
+++ pages/server-sitemap.xml/index.tsx
@@ -6,7 +6,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
   const res = await axios('https://strapi-production-9ea0.up.railway.app/api/posts');
   const { data } = res;
   const posts = data.data;
-  const urls = posts.map((post) => ({ loc: `https://cinematics.vercel.app/post/${post.attributes.slug}`, lastmod: post.attributes.updatedAt, priority: 0.6 }));
+  const urls = posts.map((post) => ({ loc: `https://cinematics.vercel.app/posts/${post.attributes.slug}`, lastmod: post.attributes.updatedAt, priority: 0.6 }));
   return getServerSideSitemap(ctx, urls);
 };
 
